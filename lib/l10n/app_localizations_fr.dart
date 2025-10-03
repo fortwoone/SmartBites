fd9228863c01fe182b87dcf004008e34ca6edc89
Zeynnz <zzeynn8@gmail.com>
2025-10-03 16:49:10 +0200

    setup pour les traductions

---
+++ lib/l10n/app_localizations_fr.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for French (`fr`).
+class AppLocalizationsFr extends AppLocalizations {
+  AppLocalizationsFr([String locale = 'fr']) : super(locale);
+
+  @override
+  String get appTitle => 'Flutter Démo';
+
+  @override
+  String get homePageTitle => 'Page d\'accueil';
+
+  @override
+  String get pushButtonMessage => 'Vous avez appuyé sur le bouton autant de fois :';
+
+  @override
+  String get increment => 'Incrémenter';
+}
