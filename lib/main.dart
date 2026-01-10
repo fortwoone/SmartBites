6624ba7798773050e8453c320cf04b6225b454e6
ElAbdos <abdeloffipro@gmail.com>
2026-01-10 23:45:39 +0100

    Renommage des fichiers de localisation et ajout de nouveaux ViewModels pour l'historique, la recherche de produits et les listes de courses

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:smartbites/l10n/app_localizations.dart';
 import 'package:smartbites/providers/app_providers.dart';
+import 'package:smartbites/views/auth/login_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'viewmodels/auth_viewmodel.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,15 +38,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SmartBites',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('En Cours .. '),
-        ),
-      ),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fr'),
+        Locale('en'),
+      ],
+      home: const AuthWrapper(),
+    );
+  }
+}
+
+class AuthWrapper extends ConsumerWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authViewModelProvider);
+
+    return authState.when(
+      data: (user) {
+        if (user != null) {
+          return const Scaffold(body: Center(child: Text("Home Screen")));
+        } else {
+          return const LoginPage();
+        }
+      },
+      error: (e, st) => Scaffold(body: Center(child: Text('Erreur: $e'))),
+      loading: () => const Scaffold(body: Center(child: CircularProgressIndicator())),
     );
   }
 }
